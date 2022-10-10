8058e883f4682f2fa056dbc02241ee921fe45864
erkan <erkanekmekci5@gmail.com>
2022-10-10 23:37:38 +0300

    scss

--- react03/rc03_sass/src/components/Card.jsx
+++ react03/rc03_sass/src/components/Card.jsx
@@ -1,5 +1,19 @@
-const Card = () => {
-  return <h1>Card</h1>;
+import CardStyle from "../scss/card.module.scss";
+
+const Card = ({data}) => {
+ return <div className={CardStyle.container}>
+  {data.map((item) =>{
+    return(
+      <div>
+        <h1>{name}</h1>
+      </div>
+    )
+  })
+
+  
+  }
+
+  </div>
 };
 
 export default Card;
--- react03/rc03_sass/src/components/Footer.jsx
+++ react03/rc03_sass/src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import Footer
+
 const Footer = () => {
   return (
     <footer>
