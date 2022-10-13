08db7b58d0c4cfe9caaaf257afef49d82a3e7b65
erkan <erkanekmekci5@gmail.com>
2022-10-13 22:37:04 +0300

    event

---
+++ react5/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -0,0 +1,12 @@
+const KeyboardClipboard = () => {
+  return <div className="container text-center">KeyboardClipboard
+  <h1>CLİPBOARD EVENTS</h1>
+
+
+  
+</div>
+
+
+};
+
+export default KeyboardClipboard;
