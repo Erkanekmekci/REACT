2fc7a6b8a9a846ae459b52c57235f3bcb0876bee
erkan <erkanekmekci5@gmail.com>
2022-10-17 23:26:16 +0300

    new data

--- react06/rc08_lifecycle-useEffect/src/components/UseEffectHook.jsx
+++ react06/rc08_lifecycle-useEffect/src/components/UseEffectHook.jsx
@@ -32,6 +32,42 @@ import { useState, useEffect } from "react";
 
 const UseEffectHook = () => {
     const [count, setCount] = useState(0);
+
+    //? componentDidMount
+    //! fetch, asyn-await ,localStorage, setTimeout, setInterval();
+    // useEffect(() => {
+    //   console.log("Mounting");
+    //   setTimeout(() => {
+    //     alert("Data Fetched");
+    //   }, 3000);
+    // }, []);
+
+    //?componentDidMount + componentDidUpdate
+    // useEffect(() => {
+    //   console.log("Mounting + Updating");
+    //   setTimeout(() => {
+    //     alert("Data Fetched");
+    //   }, 1000);
+    // }, [count]);
+
+    //?componentDidUnmount
+    const fetchData = () => {
+        console.log("Data Fetched");
+    };
+
+    useEffect(() => {
+        //! ComponentDidMount
+        const timerId = setInterval(fetchData, 1000);
+        console.log("Mounting");
+
+        return () => {
+            //! componentWillUnmount
+            clearInterval(timerId);
+            console.log("Unmounting");
+        };
+    }, []);
+
+    console.log("Rendering");
     return (
         <div className="container text-center">
             <h1 className="text-danger">USE EFFECT</h1>
--- react06/rc08_lifecycle-useEffect/src/components/user.jsx
+++ react06/rc08_lifecycle-useEffect/src/components/user.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
-const user = () => {
-    fetch("https://randomuser.me/api/").then
-  return (
-    <div>
-        <h1>Name</h1>
-        <img src="" alt="" />
-        <p>email</p>
-        <h5>DOB</h5>
-        <button></button>
-    </div>
-  )
-}
+const User = () => {
+    const [user, setUser] = useState("");
 
-export default user
+    const getUser = () => {
+        fetch("https://randomuser.me/api")
+            .then((res) => res.json())
+            .then((data) => setUser(data.results[0]));
+    };
+
+    useEffect(() => {
+        //?componentDidMount
+        getUser();
+    }, []);
+
+    console.log(user);
+    const { name, dob, email, picture } = user;
+
+    return (
+        <div>
+            <h1>
+                {/* Optional Chaining */}
+                {/* name?.first?.second */}
+                {name?.first} {name?.last}
+            </h1>
+            <img className="rounded-circle" src={picture?.large} alt="img" />
+            <p>{email}</p>
+            <h5>{new Date(dob?.date).toLocaleDateString("tr-TR")}</h5>
+            <button className="btn btn-danger" onClick={getUser}>
+                Get User
+            </button>
+        </div>
+    );
+};
+
+export default User;
